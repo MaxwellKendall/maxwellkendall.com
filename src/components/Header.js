8a019cbd9f084a5e9f00e7af50267b7017e402f7
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2018-10-13 12:27:32 -0400

    renaming files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import styles from './navigation.module.css'
+import styles from './header.module.css'
 
 export default () => (
   <nav role="navigation">
