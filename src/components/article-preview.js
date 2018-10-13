2e30018a167fa413673c4fd799d28192c8e040de
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2018-10-13 12:41:47 -0400

    removing error

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -6,15 +6,17 @@ import styles from './article-preview.module.css'
 
 export default ({ article }) => (
   <div className={styles.preview}>
-    <Img alt="" sizes={article.heroImage.sizes} />
+    {article.heroImage && (
+      <Img alt="" sizes={article.heroImage.sizes} />
+    )}
     <h3 className={styles.previewTitle}>
       <Link to={`/blog/${article.slug}`}>{article.title}</Link>
     </h3>
     <small>{article.publishDate}</small>
     <p
       dangerouslySetInnerHTML={{
-        __html: article.description.childMarkdownRemark.html,
+        __html: article.description.childMarkdownRemark.html
       }}
     />
   </div>
-)
+);
