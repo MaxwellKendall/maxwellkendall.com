c8357801ed7efa0c7c9a5b2cf53c5fc65ac6e792
Maxwell Kendall <maxwellkendall@Maxwells-MacBook-Pro.local>
2018-11-29 07:57:24 -0500

    implements search for blog post

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,39 @@ import ArticlePreview from '../components/article-preview'
 import Header from '../components/Header'
 
 class RootIndex extends React.Component {
+  state = {
+    searchTerm: '',
+    posts: []
+  };
+
+  componentDidMount() {
+    this.setState({ posts: get(this, 'props.data.allContentfulBlogPost.edges') });
+  }
+
+  filterPosts = () => {
+    const posts = this.props.data.allContentfulBlogPost.edges.filter((post) => {
+      return (
+        post.node.tags ? post.node.tags.some((tag) => tag.includes(this.state.searchTerm)) : false ||
+        post.node.title.includes(this.state.searchTerm)
+      );
+    });
+
+    this.setState({ posts });
+  };
+
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allContentfulBlogPost.edges')
     const [author] = get(this, 'props.data.allContentfulPerson.edges')
+    console.log('state: ', this.state.posts);
     return (
       <div style={{ background: '#fff' }}>
         <Header logo={this.props.data.allContentfulAsset} />
         <Helmet title={siteTitle} />
+        <input type="text" id="search" value={this.state.searchTerm} onChange={(e) => this.setState({ searchTerm: e.target.value })} placeholder="Search for post" />
+        <button type="submit" onClick={this.filterPosts}>Search</button>
         <div className="wrapper">
           <ul className="article-list">
-            {posts.map(({ node }) => {
+            {this.state.posts.map(({ node }) => {
               return (
                 <li key={node.slug}>
                   <ArticlePreview article={node} />
