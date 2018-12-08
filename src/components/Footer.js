f6b58225d8ad998265aa5030e27025954aa0efe8
Maxwell Kendall <maxwellkendall@Maxwells-MacBook-Pro.local>
2018-12-08 12:07:13 -0500

    cleaning up footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import styles from "./footer.module.css"
 
 export const Footer = () => (
   <div className={styles.footer}>
-      <p>&#169;{`Copyright Maxwell Kendall ${new Date().getFullYear()}`}</p>
+      <p>&#169; {`Copyright Maxwell Kendall ${new Date().getFullYear()}`}</p>
       <div className={styles.svgs}>
         <FontAwesomeIcon icon={["fab", "linkedin"]} />
         <FontAwesomeIcon icon={["fab", "github"]} />
