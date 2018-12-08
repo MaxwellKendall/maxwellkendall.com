2a6371cccfa0678b529fe0b591053ea95cd91bac
Maxwell Kendall <maxwellkendall@Maxwells-MacBook-Pro.local>
2018-12-08 16:11:17 -0500

    implementing css grid and  datalist element

--- src/components/Search.js
+++ src/components/Search.js
@@ -17,11 +17,11 @@ export const Search = ({
 }) => {
   return (
     <div className={styles.search}>
-      <input className={styles.search__input} type="text" id="search" value={searchTerm} onChange={updateSearchTerm} placeholder="Search for post" />
+      <input className={styles.search__input} list="tags" type="text" id="search" value={searchTerm} onChange={updateSearchTerm} placeholder="Search for post" />
       <button className={styles.search__button} type="submit" onClick={onSubmit}>Search</button>
-      <select name="auto-complete">
+      <datalist className={styles.search__options} id="tags">
         {renderAutoCompleteOptions()}
-      </select>
+      </datalist>
     </div>
   );
 }
