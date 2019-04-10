8b58bde35d64cc96a45141d86043dc0207595b1d
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-04-10 12:52:16 -0400

    renaming import to uppercase Layout

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,7 +4,7 @@ import get from 'lodash/get'
 import Img from 'gatsby-image'
 import { graphql } from 'gatsby'
 
-import { Layout } from '../components/layout'
+import { Layout } from '../components/Layout'
 import styles from './blog-post.module.css'
 
 class BlogPostTemplate extends Component {
