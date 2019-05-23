bb13fb5b1cb1dbde5148317c4e6e82e79259c614
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-05-23 10:02:06 -0400

    updating python deploy script

--- upload_portfolio_lambda.py
+++ upload_portfolio_lambda.py
@@ -3,14 +3,26 @@ import boto3, zipfile, StringIO, mimetypes
 def lambda_handler(event, context):
     s3 = boto3.resource("s3")
     sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:304945936761:deployMaxwellkendall')
+    
+    location = {
+      "bucketName": "maxwellkendall.build",
+      "objectKey": "maxwellkendall-build"
+    }
     
     try:
-      topic = sns.Topic('arn:aws:sns:us-east-1:304945936761:deployMaxwellkendall')
-      build_bucket = s3.Bucket("maxwellkendall.build")
+      job = event.get("CodePipeline.job")
+      if job:
+        for artifact in job["data"]["inputArtifacts"]:
+          if artifact["name"] == "BuildArtifact":
+            location = artifact["location"]["s3Location"]
+      build_bucket = s3.Bucket(location["bucketName"])
       prod_bucket = s3.Bucket("maxwellkendall.com")
       inMemoryBuildZip = StringIO.StringIO()
       
-      build_bucket.download_fileobj("maxwellkendall-build", inMemoryBuildZip)
+      build_bucket.download_fileobj(location["objectKey"], inMemoryBuildZip)
+      
+      print "Building portfolio from", str(location)
       
       with zipfile.ZipFile(inMemoryBuildZip) as myzip:
         for nm in myzip.namelist():
@@ -20,12 +32,22 @@ def lambda_handler(event, context):
             if mimetypes.guess_type(nm)[0] != None:
               prod_bucket.upload_fileobj(obj, nm[7:],
               ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-              print nm, mimetypes.guess_type(nm)
             else:
               prod_bucket.upload_fileobj(obj, nm[7:])
             prod_bucket.Object(nm[7:]).Acl().put(ACL='public-read')
       
       topic.publish(Subject="Maxwellkendall.com | Successful Deploy", Message="Maxwellkendall.com was just deployed")
+      if job:
+        codepipeline = boto3.client("codepipeline")
+        codepipeline.put_job_success_result(jobId=job["id"])
     except:
-       topic.publish(Subject="Maxwellkendall.com | Failed Deploy", Message="Maxwellkendall.com just failed to deploy") 
-       raise
+      if job:
+        codepipeline = boto3.client("codepipeline")
+        codepipeline.put_job_failure_result(jobId=job["id"], failureDetails={
+          "type": "JobFailed",
+          "message": "Sorry, job failed",
+          "externalExecutionId": "123"
+        })
+      
+      topic.publish(Subject="Maxwellkendall.com | Failed Deploy", Message="Maxwellkendall.com just failed to deploy") 
+      raise
