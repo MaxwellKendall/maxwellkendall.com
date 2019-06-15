fd95c0473c2cc7182a5c9a32224c92770aec1716
Maxwell Kendall <maxwellkendall@Maxwells-MacBook-Pro.local>
2019-06-15 12:59:40 -0400

    test

--- upload_portfolio_lambda.py
+++ upload_portfolio_lambda.py
@@ -43,7 +43,7 @@ def lambda_handler(event, context):
         DistributionId=cfId,
         InvalidationBatch={
           'Paths': {
-              'Quantity': 1,
+              'Quantity': 123,
               'Items': [
                   '/*',
               ]
