85f2682911941a5b5290e6ab8caf0d98f58adf9a
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-11-20 16:37:48 -0500

    basic styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,18 @@ import React from "react";
 import { Link, graphql } from "gatsby";
 import Img from 'gatsby-image';
 
+import "../styles/index.scss";
+
 const RootIndex = ({ data }) => {
   const { edges: posts } = data.allMdx
   return (
-    <div>
-      <h1>Awesome MDX Blog</h1>
-      <ul>
+    <div className="main">
+      <div className="header">
+        <h1>Awesome MDX Blog</h1>
+      </div>
+      <ul className="blog-list">
         {posts.map(({ node: post }) => (
-          <li key={post.id}>
+          <li className="blog-post__preview" key={post.id}>
             <Link to={post.fields.slug}>
               <h2>{post.frontmatter.title}</h2>
               <Img fluid={post.frontmatter.featuredImage.childImageSharp.fluid} />
@@ -18,6 +22,9 @@ const RootIndex = ({ data }) => {
           </li>
         ))}
       </ul>
+      <div className="footer">
+        Maxwell Kendall
+      </div>
     </div>
   )
 }
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,9 +4,17 @@ import { MDXRenderer } from 'gatsby-plugin-mdx';
 
 export default function PageTemplate({ data: { mdx } }) {
   return (
-    <div>
-      <h1>{mdx.frontmatter.title}</h1>
-      <MDXRenderer>{mdx.body}</MDXRenderer>
+    <div className="main">
+      <div className="header">
+        <h1>Awesome MDX Blog</h1>
+      </div>
+      <div className="blog-post">
+        <h1>{mdx.frontmatter.title}</h1>
+        <MDXRenderer>{mdx.body}</MDXRenderer>
+      </div>
+      <div className="footer">
+        Maxwell Kendall
+      </div>
     </div>
   );
 }
