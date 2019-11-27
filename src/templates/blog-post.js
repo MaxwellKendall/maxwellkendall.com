130b164898a2f05a83af2c872c292e17de0ca5cb
582199 <maxkendall@C02WC0JPHTDG.local>
2019-11-27 18:31:36 -0500

    adding footer to blog posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,18 +2,16 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import { Header } from "../components/blog/Header";
+import { Footer } from '../components/blog/Footer';
 
 export default function PageTemplate({ data: { mdx } }) {
   return (
     <div className="main">
       <Header />
       <div className="blog-post markdown-body">
-        <h1>{mdx.frontmatter.title}</h1>
         <MDXRenderer>{mdx.body}</MDXRenderer>
       </div>
-      <div className="footer">
-        Maxwell Kendall
-      </div>
+      <Footer />
     </div>
   );
 }
