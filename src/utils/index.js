f9ca95371a9eb109299fd74181ebd29b925910ac
582199 <maxkendall@C02WC0JPHTDG.local>
2019-11-29 10:59:25 -0500

    removing log statement

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,6 @@
 export const getFontColor = (izOffHrs) => {
-    const test = izOffHrs
+    const rtrn = izOffHrs
         ? '#6DA7B5'
         : '#4C6063';
-    console.log("TEST", test);
-    return test;
+    return rtrn;
 };
