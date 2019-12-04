89c9b0091d512ea8701194e85cba3dff03d8f018
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-12-04 13:02:31 -0500

    refactor

--- src/components/blog/Footer.js
+++ src/components/blog/Footer.js
@@ -8,10 +8,10 @@ export const Footer = ({ izOffHrs }) => {
     <div className="footer">
       <div className="svgs">
         <a href="https://github.com/MaxwellKendall">
-          <FontAwesomeIcon style={{ color: fontColor }} icon={faGithub} />
+          <FontAwesomeIcon style={{ color: getFontColor(izOffHrs) }} icon={faGithub} />
         </a>
         <a href="https://www.linkedin.com/in/maxwellkendall/">
-          <FontAwesomeIcon style={{ color: fontColor }} icon={faLinkedinIn} />
+          <FontAwesomeIcon style={{ color: getFontColor(izOffHrs) }} icon={faLinkedinIn} />
         </a>
       </div>
       <p style={{ color: getFontColor(izOffHrs) }}>&#169; {`Copyright Maxwell Kendall ${new Date().getFullYear()}`}</p>
