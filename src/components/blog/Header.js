03e9bb1cb5492e806fcf202f60105a21abe90fab
Maxwell Kendall <Maxwell.N.Kendall@gmail.com>
2023-02-11 18:17:42 -0500

    header wip

--- src/components/blog/Header.js
+++ src/components/blog/Header.js
@@ -9,26 +9,30 @@ import { getImage } from "../../graphql/index";
 export const Header = ({ izOffHrs }) => {
   const image = getImage(izOffHrs);
   return (
-    <div className="header w-full mx-auto max-w-5xl mb-10 md:mb-20">
-        <Link to="/" className="flex md:flex-start justify-center items-center pt-10">
-          <div className="header__bio w-1/4 flex justify-center lg:w-1/12">
-            <Img alt="headshot" image={image} className="w-full rounded-xl p-10" imgStyle={{ borderRadius: '50%' }} />
-          </div>
-      </Link>
-      <Link to="/">
-        <h1 className="text-3xl pl-2 text-center uppercase">Maxwell Kendall</h1>
-      </Link>
-        <ul className="flex w-full justify-center">
-          <li>
-            <Link className="tex-3xl font-bold pr-2" to="/blog">BLOG</Link>
-          </li>
-          <li>
-            <Link className="tex-3xl font-bold pr-2" to="/portfolio">PORTFOLIO</Link>
-          </li>
-          <li>
-            <a className="tex-3xl font-bold" target="_blank" href="https://maxwell-kendall-resume.s3.amazonaws.com/MAXWELL_KENDALL_Resume.PDF">RESUME <FontAwesomeIcon icon={faExternalLinkAlt} /></a>
-          </li>
-        </ul>
-    </div>
+    <div className="header flex justify-center py-10">
+      {/* <div className="flex w-full justify-center"> */}
+        <Link to="/" className="flex">
+            <div className="header__bio">
+              <Img alt="headshot" image={image} className="w-full rounded-xl p-10" imgStyle={{ borderRadius: '50%' }} />
+            </div>
+        </Link>
+        <div className="pl-4 flex flex-col my-auto">
+          <Link to="/" className='flex self-end'>
+            <h1 className="text-3xl text-center uppercase">Maxwell Kendall</h1>
+          </Link>
+          <ul className="flex">
+            <li>
+              <Link className="text-xl font-bold pr-2" to="/blog">BLOG</Link>
+            </li>
+            <li>
+              <Link className="text-xl font-bold pr-2" to="/portfolio">PORTFOLIO</Link>
+            </li>
+            <li>
+              <a className="text-xl font-bold" target="_blank" href="https://maxwell-kendall-resume.s3.amazonaws.com/MAXWELL_KENDALL_Resume.PDF">RESUME <FontAwesomeIcon icon={faExternalLinkAlt} /></a>
+            </li>
+          </ul>
+        </div>
+      {/* </div> */}
+  </div>
   );
 };
