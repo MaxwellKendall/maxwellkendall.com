7bdc0633e945c3d948dcdabd4b52d653242ee520
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2024-12-07 01:08:17 -0500

    wip: code highlighting

---
+++ gatsby-browser.js
@@ -0,0 +1,7 @@
+// require('prismjs/themes/prism-solarizedlight.css');
+// require('prismjs/themes/prism-dark.css');
+// require('prismjs/plugins/line-numbers/prism-line-numbers.css');
+// require('prismjs/plugins/command-line/prism-command-line.css');
+// require('prismjs/themes/prism-tomorrow.css');
+// require('prismjs/plugins/line-numbers/prism-line-numbers.css');
+// require('../styles/prism-overrides.css');
